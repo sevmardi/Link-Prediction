bce04ee171f50924b46b0f85adecc1e88ae538fa
MicT <mixalis_1992@hotmail.com>
2017-11-17 18:27:57 +0100

    Added code for creating dataframe

--- UC-Irvine/UC-Irvine_load_metrics.py
+++ UC-Irvine/UC-Irvine_load_metrics.py
@@ -1,24 +1,38 @@
 import pickle
-
+import pandas as pd
 """
 Read the metrics.
 
 """
 
-with open('/UC-Irvine/adamicAdar_UC-Irvine.pkl', 'rb') as y:
+with open('UC-Irvine/adamicAdar_UC-Irvine.pkl', 'rb') as y:
     adamicAdarList = pickle.load(y)
 
-with open('/UC-Irvine/commonNeighbors_UC-Irvine.pkl', 'rb') as y:
+with open('UC-Irvine/commonNeighbors_UC-Irvine.pkl', 'rb') as y:
     commonNeighborsList = pickle.load(y)
 
-with open('/UC-Irvine/rootedPageRankList_UC-Irvine.pkl', 'rb') as y:
+with open('UC-Irvine/rootedPageRankList_UC-Irvine.pkl', 'rb') as y:
     rootedPageRankList = pickle.load(y)
 
-with open('/UC-Irvine/jaccard_UC-Irvine.pkl', 'rb') as y:
+with open('UC-Irvine/jaccard_UC-Irvine.pkl', 'rb') as y:
     jaccardList = pickle.load(y)
 
-with open('/UC-Irvine/resAllocation_UC-Irvine.pkl', 'rb') as y:
+with open('UC-Irvine/resAllocation_UC-Irvine.pkl', 'rb') as y:
     resAllocationList = pickle.load(y)
 
-with open('/UC-Irvine/assocStrength_UC-Irvine.pkl', 'rb') as y:
+with open('UC-Irvine/assocStrength_UC-Irvine.pkl', 'rb') as y:
     assocStrengthList = pickle.load(y)
+    
+with open('UC-Irvine/labels_UC-Irvine.pkl', 'rb') as y:
+    labels = pickle.load(y)
+    
+dataset = pd.DataFrame(
+    {'Adamic Adar': adamicAdarList,
+     'Common Neighbors': commonNeighborsList,
+     'Rooted PageRank': rootedPageRankList,
+     'Jaccard': jaccardList,
+     'Resource Allocation': resAllocationList,
+     'Association Strength': assocStrengthList,
+     'Labels': labels,
+    })
+3
